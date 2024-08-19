97221a13cabff936398d1efb28ce616622072866
hostsupreme-gitrepo <73408082+hostsupreme-gitrepo@users.noreply.github.com>
2024-08-18 19:03:08 -0700

    new homepahe

--- frontend/src/app/[lang]/components/Hero.tsx
+++ frontend/src/app/[lang]/components/Hero.tsx
@@ -34,11 +34,11 @@ interface HeroProps {
 }
 
 export default function Hero({ data }: HeroProps) {
-  const imgUrl = getStrapiMedia(data.picture.data.attributes.url);
+  //const imgUrl = getStrapiMedia(data.picture.data.attributes.url);
 
   return (
     <section className="dark:bg-black dark:text-gray-100">
-      <div className="container flex flex-col justify-center p-6 mx-auto sm:py-12 lg:py-24 lg:flex-row lg:justify-between">
+      <div className="container flex flex-col bg-[url('../uploads/0818241539_HDR_2_50_30cbccf403.jpg')]  justify-center p-6 mx-auto sm:py-12 lg:py-24 lg:flex-row lg:justify-between">
         <div className="flex flex-col justify-center p-6 text-center rounded-lg lg:max-w-md xl:max-w-lg lg:text-left">
           <HighlightedText
             text={data.title}
@@ -66,17 +66,18 @@ export default function Hero({ data }: HeroProps) {
             ))}
           </div>
         </div>
-        <div className="flex items-center justify-center p-6 mt-8 lg:mt-0 h-72 sm:h-80 lg:h-96 xl:h-112 2xl:h-128">
+{/*         <div className="flex items-center justify-center p-6 mt-8 lg:mt-0 h-72 sm:h-80 lg:h-96 xl:h-112 2xl:h-128">
           <Image
             src={imgUrl || ""}
             alt={
               data.picture.data.attributes.alternativeText || "none provided"
             }
-            className="object-contain h-72 sm:h-80 lg:h-96 xl:h-112 2xl:h-128 "
+            // className="object-cover h-72 sm:h-80 lg:h-96 xl:h-112 2xl:h-128 "
+            className=" left-0  h-72 sm:h-80 lg:h-96 xl:h-112 2xl:h-128 "
             width={600}
             height={600}
           />
-        </div>
+        </div> */}
       </div>
     </section>
   );
